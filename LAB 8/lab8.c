56726de1f41e3072d383679fd3e9f3fd27a54bc8
Sagar <sagars.cs24@bmsce.ac.in>
2025-12-26 21:40:15 +0530

    Add files via upload
    LAB 8

---
+++ LAB 8/lab8.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+
+struct node* createNode(int item) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = item;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+
+struct node* insert(struct node* root, int item) {
+    if (root == NULL)
+        return createNode(item);
+
+    if (item < root->data)
+        root->left = insert(root->left, item);
+    else if (item > root->data)
+        root->right = insert(root->right, item);
+
+    return root;
+}
+
+void inorder(struct node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+
+void preorder(struct node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+
+void postorder(struct node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    struct node* root = NULL;
+    int n, value, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    printf("\nInorder Traversal: ");
+    inorder(root);
+
+    printf("\nPreorder Traversal: ");
+    preorder(root);
+
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+
+    return 0;
+}
